bd6cee7f94abca8ad2ac8c3145bc34985e448b96
TCSBANK\d.avilov <d.avilov@tinkoff.ru>
2022-03-27 14:57:00 +0300

    docs: add documentation for class ConsolePrinter

--- src/main/java/ConsolePrinter.java
+++ src/main/java/ConsolePrinter.java
@@ -1,5 +1,14 @@
+import java.io.*;
+/**
+ * Class provides methods for printing input messages
+ * @author d.avilov
+ */
 public class ConsolePrinter {
-    public static void print(String message) {
+    /**
+     * This Method is used to print input messages into console
+     * @param message a string that will be print
+     */
+    public static void print(String message){
         System.out.println(message);
     }
 }
