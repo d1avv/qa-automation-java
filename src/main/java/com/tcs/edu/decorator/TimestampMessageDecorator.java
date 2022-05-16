88246d7cab4088c274a8757dfc7229c31e2a34ec
TCSBANK\d.avilov <d.avilov@tinkoff.ru>
2022-05-16 16:33:52 +0300

    feat: practice 7. 7-1 обработка NPE для MessageService.processMessage

--- src/main/java/com/tcs/edu/decorator/TimestampMessageDecorator.java
+++ src/main/java/com/tcs/edu/decorator/TimestampMessageDecorator.java
@@ -4,12 +4,14 @@ import java.time.Instant;
 
 /**
  * Class provides decorating of the message
+ *
  * @author d.avilov
  */
 
 public class TimestampMessageDecorator {
     /**
      * This Method is used to decorate input message by adding current time stamp
+     *
      * @param message a string that will be decorated
      * @return decorated message
      */
@@ -18,7 +20,7 @@ public class TimestampMessageDecorator {
     public static final int PAGE_SIZE = 6;
 
     public static String decorate(String message) {
-        messageCount ++;
+        messageCount++;
         final var decoratedMessage = String.format("%1$s %2$s %3$s", messageCount, Instant.now(), message);
         if (messageCount > 0 & messageCount % 2 == 0) {
             return String.format("%1$s %2$s", decoratedMessage, "\n---");
